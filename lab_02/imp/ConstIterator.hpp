9031593132397b0a7ec4fcc240fe2f75f561faf7
Inspirate789 <andreysapozhkov535@gmail.com>
2022-04-18 19:54:49 +0300

    Add the implementation of ConstIterator

---
+++ lab_02/imp/ConstIterator.hpp
@@ -0,0 +1,226 @@
+#ifndef _CONST_ITERATOR_HPP_
+#define _CONST_ITERATOR_HPP_
+
+#include "ConstIterator.h"
+#include "Exceptions.h"
+
+#pragma region Constructors
+template <typename Type>
+ConstIterator<Type>::ConstIterator(const Vector<Type> &vector) noexcept
+{
+    ptr = vector.data;
+    size = vector.size;
+    index = 0;
+}
+
+template <typename Type>
+ConstIterator<Type>::ConstIterator(const ConstIterator<Type> &iterator) noexcept
+{
+    ptr = iterator.ptr;
+    size = iterator.size;
+    index = iterator.index;
+}
+#pragma endregion Constructors
+
+#pragma region Operators
+template <typename Type>
+ConstIterator<Type>::operator bool() const
+{
+    expiredCheck(__LINE__);
+
+    return size && index < size;
+}
+
+template <typename Type>
+ConstIterator<Type> &ConstIterator<Type>::operator=(const ConstIterator<Type> &iterator) noexcept
+{
+    ptr.reset(iterator.ptr);
+    size = iterator.size;
+    index = iterator.index;
+}
+
+template <typename Type>
+const Type &ConstIterator<Type>::operator*() const
+{
+    expiredCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    return *getCurPtr();
+}
+
+template <typename Type>
+const Type *ConstIterator<Type>::operator->() const
+{
+    expiredCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    return getCurPtr();
+}
+
+template <typename Type>
+ConstIterator<Type> ConstIterator<Type>::operator+(const size_t diff) const
+{
+    expiredCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    ConstIterator<Type> res(*this);
+    res.index += diff;
+    return res;
+}
+
+template <typename Type>
+ConstIterator<Type> ConstIterator<Type>::operator-(const size_t diff) const
+{
+    expiredCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    ConstIterator<Type> res(*this);
+    res.index -= diff;
+    return res;
+}
+
+template <typename Type>
+ConstIterator<Type> &ConstIterator<Type>::operator+=(const size_t diff)
+{
+    expiredCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    index += diff;
+    return this;
+}
+
+template <typename Type>
+ConstIterator<Type> &ConstIterator<Type>::operator-=(const size_t diff)
+{
+    expiredCheck(__LINE__);
+    indexCheck(__LINE__);
+
+    index -= diff;
+    return this;
+}
+
+template <typename Type>
+ConstIterator<Type> & ConstIterator<Type>::operator++()
+{
+    expiredCheck(__LINE__);
+
+    ++index;
+    return *this;
+}
+
+template <typename Type>
+ConstIterator<Type> ConstIterator<Type>::operator++(int)
+{
+    expiredCheck(__LINE__);
+
+    ConstIterator<Type> tmp(*this);
+    ++(*this);
+    return tmp;
+}
+
+template <typename Type>
+ConstIterator<Type> & ConstIterator<Type>::operator--()
+{
+    expiredCheck(__LINE__);
+
+    --index;
+    return *this;
+}
+
+template <typename Type>
+ConstIterator<Type> ConstIterator<Type>::operator--(int)
+{
+    expiredCheck(__LINE__);
+
+    ConstIterator<Type> tmp(*this);
+    --(*this);
+    return tmp;
+}
+
+template <typename Type>
+bool ConstIterator<Type>::operator==(const ConstIterator<Type> &iterator) const
+{
+    expiredCheck(__LINE__);
+    iterator.expiredCheck(__LINE__);
+
+    return getCurPtr() == iterator.getCurPtr();
+}
+
+template <typename Type>
+bool ConstIterator<Type>::operator!=(const ConstIterator<Type> &iterator) const
+{
+    expiredCheck(__LINE__);
+    iterator.expiredCheck(__LINE__);
+
+    return getCurPtr() != iterator.getCurPtr();
+}
+
+template <typename Type>
+bool ConstIterator<Type>::operator>(const ConstIterator<Type> &iterator) const
+{
+    expiredCheck(__LINE__);
+    iterator.expiredCheck(__LINE__);
+
+    return getCurPtr() > iterator.getCurPtr();
+}
+
+template <typename Type>
+bool ConstIterator<Type>::operator>=(const ConstIterator<Type> &iterator) const
+{
+    expiredCheck(__LINE__);
+    iterator.expiredCheck(__LINE__);
+
+    return getCurPtr() >= iterator.getCurPtr();
+}
+
+template <typename Type>
+bool ConstIterator<Type>::operator<(const ConstIterator<Type> &iterator) const
+{
+    expiredCheck(__LINE__);
+    iterator.expiredCheck(__LINE__);
+
+    return getCurPtr() < iterator.getCurPtr();
+}
+
+template <typename Type>
+bool ConstIterator<Type>::operator<=(const ConstIterator<Type> &iterator) const
+{
+    expiredCheck(__LINE__);
+    iterator.expiredCheck(__LINE__);
+
+    return getCurPtr() <= iterator.getCurPtr();
+}
+#pragma endregion Operators
+
+#pragma region ProtectedMethods
+template <typename Type>
+Type *ConstIterator<Type>::getCurPtr() const
+{
+    std::shared_ptr<Type[]> tmp = ptr.lock();
+    return tmp.get() + index;
+}
+
+template <typename Type>
+void ConstIterator<Type>::expiredCheck(const size_t line) const
+{
+    if (ptr.expired())
+    {
+        time_t cur_time = time(NULL);
+        throw ExpiredException(ctime(&cur_time), __FILE__, line,
+                               typeid(*this).name(), __FUNCTION__);
+    }
+}
+
+template <typename Type>
+void ConstIterator<Type>::indexCheck(const size_t line) const
+{
+    if (index >= size)
+    {
+        time_t cur_time = time(NULL);
+        throw OutOfRangeException(ctime(&cur_time), __FILE__, line,
+                                  typeid(*this).name(), __FUNCTION__);
+    }
+}
+#pragma endregion ProtectedMethods
+
+#endif
--- lab_02/inc/ConstIterator.h
+++ lab_02/inc/ConstIterator.h
@@ -1,14 +1,60 @@
 #ifndef _CONST_ITERATOR_H_
 #define _CONST_ITERATOR_H_
 
+#include <memory>
+
+#include "BaseIterator.h"
+
 template <typename Type>
-class ConstIterator
+class Vector;
+
+template <typename Type>
+class ConstIterator : public BaseIterator, public std::iterator<std::random_access_iterator_tag, Type>
 {
-private:
-    /* data */
+    friend class Vector<Type>;
+
 public:
-    ConstIterator(/* args */);
-    ~ConstIterator();
+    ConstIterator(const Vector<Type> &vector) noexcept;
+    ConstIterator(const ConstIterator<Type> &iterator) noexcept;
+
+    ~ConstIterator() = default;
+
+    operator bool() const;
+    ConstIterator<Type> &operator=(const ConstIterator<Type> &iterator) noexcept;
+
+    const Type &operator*() const;
+    const Type *operator->() const;
+
+    ConstIterator<Type> operator+(const size_t diff) const;
+    ConstIterator<Type> operator-(const size_t diff) const;
+
+    ConstIterator<Type> &operator+=(const size_t diff);
+    ConstIterator<Type> &operator-=(const size_t diff);
+
+    ConstIterator<Type> &operator++();
+    ConstIterator<Type> operator++(int);
+
+    ConstIterator<Type> &operator--();
+    ConstIterator<Type> operator--(int);
+
+    bool operator==(const ConstIterator<Type> &iterator) const;
+    bool operator!=(const ConstIterator<Type> &iterator) const;
+
+    bool operator>(const ConstIterator<Type> &iterator) const;
+    bool operator>=(const ConstIterator<Type> &iterator) const;
+
+    bool operator<(const ConstIterator<Type> &iterator) const;
+    bool operator<=(const ConstIterator<Type> &iterator) const;
+
+protected:
+    Type *getCurPtr() const;
+    void expiredCheck(const size_t line) const;
+    void indexCheck(const size_t line) const;
+
+private:
+    std::weak_ptr<Type[]> ptr; // указатель без права собственности на объект
 };
+
+#include "ConstIterator.hpp"
 
 #endif
